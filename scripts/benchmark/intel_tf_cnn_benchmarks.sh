0b6ee23e7f1c5d1f2c5eea817f8f93119966a2af
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2019-02-13 14:08:23 -0800

    Update intel_tf_cnn_benchmarks.sh

--- scripts/benchmark/intel_tf_cnn_benchmarks.sh
+++ scripts/benchmark/intel_tf_cnn_benchmarks.sh
@@ -1,12 +1,30 @@
-# !/bin/bash
-# Usage: bash intel_tf_cnn_benchmarks <option> 
+#!/bin/bash
+#
+# Copyright (c) 2019 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+####### USAGE #########
+# bash intel_tf_cnn_benchmarks <option> 
 # 
 # By default, this runs only InceptionV3 at batch size 64. Pass "all" in the <option> 
 # position to run all networks and batch sizes in the benchmarking suite.
 # 
 # This script runs TensorFlow's CNN Benchmarks and summarizes throughput increases when  
 # using Intel optimized TensorFlow.
-# Note: you may need to edit benchmarks/scripts/tf_cnn_benchmarks/datasets.py to import _pickle instead of Cpickle
+# Note: you may need to edit benchmarks/scripts/tf_cnn_benchmarks/datasets.py to 
+# import _pickle instead of Cpickle
 
 
 # Check if "all" option was passed, set networks and batch sizes accordingly
@@ -30,7 +48,14 @@ for network in "${networks[@]}" ; do
   for bs in "${batch_sizes[@]}"; do
     echo -e "\n\n #### Starting $network and batch size = $bs ####\n\n"
 
-    time python tf_cnn_benchmarks.py --data_format NHWC --data_name synthetic --model "$network" --forward_only False -batch_size "$bs" --num_batches 50 2>&1 | tee net_"$network"_bs_"$bs"_default.log
+    time python tf_cnn_benchmarks.py \
+    --data_format NHWC \
+    --data_name synthetic \
+    --model "$network" \
+    --forward_only False \
+    --batch_size "$bs" \
+    --num_batches 50 \
+    2>&1 | tee net_"$network"_bs_"$bs"_default.log
 
   done
 done
@@ -42,7 +67,14 @@ for network in "${networks[@]}" ; do
   for bs in "${batch_sizes[@]}"; do
     echo -e "\n\n #### Starting $network and batch size = $bs ####\n\n"
 
-    time python tf_cnn_benchmarks.py --data_format NCHW --data_name synthetic --model "$network" --forward_only False -batch_size "$bs" --num_batches 10 2>&1 | tee net_"$network"_bs_"$bs"_optimized.log
+    time python tf_cnn_benchmarks.py \
+    --data_format NCHW \
+    --data_name synthetic \
+    --model "$network" \
+    --forward_only False \
+    --batch_size "$bs" \
+    --num_batches 50 \
+    2>&1 | tee net_"$network"_bs_"$bs"_optimized.log
 
   done
 done
