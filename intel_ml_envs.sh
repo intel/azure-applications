c9e2c8d1fdb44b70a564055fb3e71a9ae0ba915f
mattsonthieme <mattson.thieme@gmail.com>
2019-05-16 12:59:21 -0700

    Add pytorch 2/3 and Intel Caffe

--- intel_ml_envs.sh
+++ intel_ml_envs.sh
@@ -39,9 +39,51 @@ source deactivate
 
 # Create Python 3.6 MXNet env
 yes 'y' | conda create -n intel_mxnet_p36 -c intel python=3 numpy opencv Pillow scipy scikit-learn mxnet-mkl keras
+source activate intel_mxnet_p27
 export LD_LIBRARY_PATH=/usr/local/lib
 export OMP_NUM_THREADS=$(lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g")
 export KMP_AFFINITY=granularity=fine,compact,1,0
 export KMP_BLOCKTIME=1
 export OMP_PROC_BIND=true
 source deactivate
+
+# Create Python 2.7 PyTorch env
+yes 'y' | conda create -n intel_pytorch_p27 -c intel python=2 pip numpy pyyaml mkl setuptools cmake cffi
+source activate intel_pytorch_p27
+cd
+git clone --recursive https://github.com/intel/pytorch
+export NO_CUDA=1
+export CMAKE_PREFIX_PATH=~/anaconda3/
+cd pytorch
+python setup.py install
+source deactivate
+
+# Create Python 3.6 PyTorch env
+yes 'y' | conda create -n intel_pytorch_p36 -c intel python=3 pip numpy pyyaml mkl setuptools cmake cffi
+source activate intel_pytorch_p36
+cd
+export NO_CUDA=1
+export CMAKE_PREFIX_PATH=~/anaconda3/
+cd pytorch
+python setup.py install
+source deactivate
+
+# Create Python 2.7 IntelCaffe env
+yes 'y' | conda create -n intelcaffe_p27 -c intel python=2 pip numpy
+source activate intelcaffe_p27
+sudo apt-get update
+sudo apt-get -y install build-essential git cmake
+sudo apt-get -y install libprotobuf-dev libleveldb-dev libsnappy-dev
+sudo apt-get -y install libopencv-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get -y install --no-install-recommends libboost-all-dev
+sudo apt-get -y install libgflags-dev libgoogle-glog-dev liblmdb-dev
+sudo apt-get -y install libatlas-base-dev python-numpy python-pip
+sudo apt install numactl
+git clone https://github.com/intel/caffe ~/src/intelcaffe_p27
+cd ~/src/intelcaffe_p27
+cp Makefile.config.example Makefile.config
+export PATH=/usr/bin:$PATH
+make
+source deactivate
+
+
