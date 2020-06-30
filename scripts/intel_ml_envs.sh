58ab695008b8c0e8b96eeabc82898212f1958b3c
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2020-06-30 12:46:32 -0700

    Update intel_ml_envs.sh

--- scripts/intel_ml_envs.sh
+++ scripts/intel_ml_envs.sh
@@ -10,11 +10,9 @@
 # export PATH=${conda_path::-6}:$PATH
 export PATH=/data/anaconda/condabin/:$PATH
 
-# Add Intel channel to conda
-conda config --add channels intel
 
 #### Create Python 3 TensorFlow env ####
-yes 'y' | conda create -n intel_tensorflow_p37 -c intel python=3.7
+yes 'y' | conda create -n intel_tensorflow_p37 python=3.7
 
 # Create a post-activate script to install all packages
 TF_LOCATION=/data/anaconda/envs/intel_tensorflow_p37
@@ -41,7 +39,7 @@ fi
 EOT
 
 #### Create Python 3 MXNet env ####
-yes 'y' | conda create -n intel_mxnet_p37 -c intel python=3.7
+yes 'y' | conda create -n intel_mxnet_p37 python=3.7
 
 # Create a post-activate script to install all packages
 
@@ -63,7 +61,7 @@ then
     export KMP_SETTINGS=1
     export OMP_NUM_THREADS=$(lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g")
     export OMP_PROC_BIND=true
-    conda install -y -c intel mxnet-mkl
+    pip install mxnet-mkl
 fi
 
 EOT
@@ -71,7 +69,7 @@ EOT
 
 #### Create Python 3 PyTorch env ####
 ## Must build the environment with supporting packages first - doesn't add much extra time
-yes 'y' | conda create -n intel_pytorch_p37 -c intel python=3.7 numpy pyyaml mkl mkl-include setuptools cmake cffi typing
+yes 'y' | conda create -n intel_pytorch_p37 python=3.7
 
 # Create a post-activate script to install all packages
 
@@ -88,17 +86,12 @@ if ! conda list | grep -q '^torch';
 then
     echo "Installing Intel Optimized PyTorch..."
     echo "This is a one-time installation, and may take a minute..."
-    export NO_CUDA=1
-    export CMAKE_PREFIX_PATH=~/data/anaconda/
-    git clone --recursive https://github.com/intel/pytorch
-    cd pytorch
-    python setup.py install
-    cd
     export KMP_AFFINITY=granularity=fine,noverbose,compact,1,0
     export KMP_BLOCKTIME=1
     export KMP_SETTINGS=1
     export OMP_NUM_THREADS=$(lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g")
     export OMP_PROC_BIND=true
+    yes 'y' | conda install pytorch-cpu torchvision-cpu -c pytorch
 fi
 
 EOT
